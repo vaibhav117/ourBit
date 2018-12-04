9e97956c4c938d445043746daf9a2c77a7458aa9
Vaibhav Mathur <vaibhavmathurcode@gmail.com>
2018-12-04 23:20:10 +0530

    post request for sending data

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,58 @@ router.get('/response', (req, res) => {
 });
 
 
+router.get('/upload',(req,res) => {
+  //************************************* GOOGLE API CALL ************************************* */
+
+  /*Requires env variable : export GOOGLE_APPLICATION_CREDENTIALS="/Users/Galactica/Desktop/Projects/hack_for_good/My_First_Project-950bc161e559.json"*/
+
+  // Imports the Google Cloud client library
+  const speech = require('@google-cloud/speech');
+  const fs = require('fs');
+
+  // Your Google Cloud Platform project ID
+  const projectId = 'decent-tape-224418';
+
+  // Creates a client
+  const client = new speech.SpeechClient({
+    projectId: projectId,
+  });
+
+
+  // Reads a local audio file and converts it to base64
+  const file = req.file;
+  const audioBytes = file.toString('base64');
+
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  const audio = {
+    content: audioBytes,
+  };
+  const config = {
+    encoding: 'LINEAR16',
+    //sampleRateHertz: 16000,
+    languageCode: 'en-US',
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
+
+
+
+  // Detects speech in the audio file
+  client
+    .recognize(request)
+    .then(data => {
+      const response = data[0];
+      const transcription = response.results
+        .map(result => result.alternatives[0].transcript)
+        .join('\n');
+      console.log(`Transcription: ${transcription}`);
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+});
+
 
 module.exports = router;
